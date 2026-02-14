2bf98efcb7b1b25efa2bc248f0e2a206f51fe157
laldivane <laldivanemusic@gmail.com>
2026-02-14 18:51:29 +0300

    Fix: Exclude scripts from build and fix TS import extension

--- scripts/migrate.ts
+++ scripts/migrate.ts
@@ -1,5 +1,5 @@
 import { createClient } from '@sanity/client'
-import { rituals, loreNodes } from '../src/lib/data.ts'
+import { rituals, loreNodes } from '../src/lib/data'
 import * as dotenv from 'dotenv'
 
 dotenv.config({ path: '.env.local' })
